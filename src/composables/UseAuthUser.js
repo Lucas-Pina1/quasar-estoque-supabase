f3dc1c4873391473b5066d17b6769938fdeff89f
Lucas <lucas.pina@prodepa.pa.gov.br>
2023-04-19 11:56:00 -0300

    continuar

---
+++ src/composables/UseAuthUser.js
@@ -0,0 +1,11 @@
+export default function useAuthUser () {
+  const login = async ({ email, password }) => {}
+
+  const loginWithSocialProvider = async (provider) => {}
+
+  const logout = async () => {}
+
+  const isLoggedIn = () => {
+
+  }
+}
